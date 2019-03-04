a56d85b4956ab0fc8bfc0ae1ba91ce6e319435ec
Shawn Polson <sapols@yahoo.com>
2019-03-04 14:40:26 -0700

    Implement ARIMA detection func, minus a few TODOs

--- detect_anomalies_with_ARIMA.py
+++ detect_anomalies_with_ARIMA.py
@@ -1,4 +1,5 @@
 # Standard modules
+import os
 import datetime
 import pandas as pd
 import numpy as np
@@ -11,26 +12,31 @@ from statsmodels.tsa.seasonal import seasonal_decompose
 from statsmodels.tsa.arima_model import ARIMAResults
 from sklearn.metrics import mean_squared_error
 
+# Custom modules
+from grid_search_hyperparameters import grid_search_arima_params  #TODO: make separate file for grid search
+from grid_search_hyperparameters import grid_search_sarima_params #TODO: make separate file for grid search
+
 __author__ = 'Shawn Polson'
 __contact__ = 'shawn.polson@colorado.edu'
 
 
-def detect_anomalies_with_arima(ts, size, order, seasonal_order=(), grid_search=False, path_to_model='',
-                                 verbose=False, var_name='Value'):
-    """Detect outliers in the time series data by comparing points against [num_stds] standard deviations from a rolling mean.
+def detect_anomalies_with_arima(ts, train_size, order, seasonal_order=(), seasonal_freq=None, grid_search=False,
+                                path_to_model=None, verbose=False, var_name='Value'):
+    """Detect outliers in the time series data by comparing points against an ARIMA forecast.
 
        Inputs:
-           ts [pd Series]:         A pandas Series with a DatetimeIndex and a column for numerical values.
-           size [float]:           The percentage of data to use for training, as a float (e.g., 0.66).
-           order [tuple]:          The order hyperparameters (p,d,q) for this ARIMA model.
+           ts [pd Series]:     A pandas Series with a DatetimeIndex and a column for numerical values.
+           train_size [float]: The percentage of data to use for training, as a float (e.g., 0.66).
+           order [tuple]:      The order hyperparameters (p,d,q) for this ARIMA model.
 
 
        Optional Inputs:
-           seasonal_order [tuple]: The seasonal order hyperparameters (P,D,Q,freq) for this (S)ARIMA model.
+           seasonal_order [tuple]: The seasonal order hyperparameters (P,D,Q) for this SARIMA model. When specifying these, 'seasonal_freq' must also be given.
+           seasonal_freq [int]:    The freq hyperparameter for this SARIMA model, i.e., the number of samples that make up one seasonal cycle.
            grid_search [bool]:     When True, perform a grid search to set values for the 'order' and 'seasonal order' hyperparameters.
-                                   Note this overrides any given values. Default is False.
-           path_to_model [str]:    Path to a *.pkl file of a trained SARIMA model. When set, no training will be done because that model will be used.
-           verbose [bool]:         ??? When True, ???
+                                   Note this overrides any given (p,d,q)(P,D,Q) hyperparameter values. Default is False.
+           path_to_model [str]:    Path to a *.pkl file of a trained (S)ARIMA model. When set, no training will be done because that model will be used.
+           verbose [bool]:         ??? When True, ??? (show acf, pacf, plot residual training errors, predicted v. expected prints, )
            var_name [str]:         The name of the dependent variable in the time series.
                                    Default is 'Value'.
 
@@ -42,44 +48,63 @@ def detect_anomalies_with_arima(ts, size, order, seasonal_order=(), grid_search=
            None
 
        Example:
-           time_series_with_outliers, outliers = detect_anomalies_with_arima(time_series, order=(12, 0, 0),
-                                                                              seasonal_order=(0, 1, 0, 365),
-                                                                              size=0.8, verbose=False)
+           time_series_with_outliers, outliers = detect_anomalies_with_arima(time_series, train_size=0.8, order=(12,0,0),
+                                                                             seasonal_order=(0,1,0), seasonal_freq=365,
+                                                                             verbose=False)
     """
 
-    # Forecast with training data
-    X = ts
-    split = int(len(X) * size)
-    train, test = X[0:split], X[split:len(X)]
-    std = float(train.values.std(ddof=0))
+    # Finalize ARIMA/SARIMA hyperparameters
+    if grid_search and path_to_model is not None:
+        raise ValueError('\'grid_search\' should be False when specifying a path to a pre-trained ARIMA model.')
 
-    if grid_search:  #TODO: decide how to differentiate ARIMA vs. SARIMA for grid search
+    if (seasonal_freq is not None) and (len(seasonal_order) == 3) and (grid_search is False):
+        seasonal_order = seasonal_order + (seasonal_freq,)  # (P,D,Q,freq)
+    elif (seasonal_freq is not None) and (len(seasonal_order) != 3) and (grid_search is False):
+        raise ValueError('\'seasonal_order\' must be a tuple of 3 integers when specifying a seasonal frequency and not grid searching.')
+    elif (seasonal_freq is None) and (len(seasonal_order) == 3) and (grid_search is False):
+        raise ValueError('\'seasonal_freq\' must be given when specifying a seasonal order and not grid searching.')
+
+    if grid_search:
         if verbose:
             lag_acf = acf(ts, nlags=20)
             lag_pacf = pacf(ts, nlags=20, method='ols')
             pyplot.show()
-        pass  # TODO: grid search (make a separate file for it?)
-        #order, seasonal_order = grid_search_sarima_params(???) #TODO: can freq be found?? Or must it be given upfront?
-        print('Grid search found hyperparameters:')
-        print(str(order) + ' ' + str(seasonal_order))
+        if seasonal_freq is None:  # ARIMA grid search
+            print('No seasonal frequency was given, so grid searching ARIMA(p,d,q) hyperparameters.')
+            order = grid_search_arima_params(ts)
+            print('Grid search found hyperparameters: ' + str(order) + '\n')
+        else:  # SARIMA grid search
+            print('Seasonal frequency was given, so grid searching ARIMA(p,d,q)(P,D,Q) hyperparameters.')
+            order, seasonal_order = grid_search_sarima_params(ts, seasonal_freq)
+            print('Grid search found hyperparameters: ' + str(order) + str(seasonal_order) + '\n')
+
+    # Train or load ARIMA/SARIMA model
+    X = ts
+    split = int(len(X) * train_size)
+    train, test = X[0:split], X[split:len(X)]
+    threshold = float(train.values.std(ddof=0))  # TODO: finalize/decide std scheme (pass it in?)
 
     if len(seasonal_order) < 4:
         trained_model = ARIMA(train, order=order)
     else:
         trained_model = SARIMAX(train, order=order, seasonal_order=seasonal_order)
 
-    if path_to_model != '':
+    if path_to_model is not None:
         # load pre-trained model
         trained_model_fit = ARIMAResults.load(path_to_model)
     else:
         trained_model_fit = trained_model.fit(disp=1)
-        # save just-trained model
+        # save the just-trained model
         try:
             current_time = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
             filename = 'SARIMA_' + str(order) + '_' + str(seasonal_order) + '_' + current_time + '.pkl'
+            model_dir = 'Models/'
+            if not os.path.exists(model_dir):
+                os.makedirs(model_dir)
+            filename = model_dir + filename
             trained_model_fit.save(filename)
         except Exception as e:
-            print("Saving model failed:")
+            print('Saving model failed:')
             print(e)
 
     print(trained_model_fit.summary())
@@ -92,37 +117,45 @@ def detect_anomalies_with_arima(ts, size, order, seasonal_order=(), grid_search=
         residuals.plot(kind='kde', title='Training Model Fit Residual Error Density')
         pyplot.show()
         print(residuals.describe())
+        print('\n')
 
-    predictions = trained_model_fit.predict(start=1, end=len(X) - 1, typ='levels')
+    # Forecast with the trained ARIMA/SARIMA model
+    predictions = trained_model_fit.predict(start=1, end=len(X)-1, typ='levels')
     predict_index = pd.Index(X.index[1:len(X)])
     predictions_with_dates = pd.Series(predictions.values, index=predict_index)
     outliers = pd.Series()
-    time_series_with_outliers = pd.DataFrame({var_name: ts}) # TODO: use this correctly
+    time_series_with_outliers = pd.DataFrame({var_name: X})
     time_series_with_outliers['Outlier'] = 'False'
 
-    # Label outliers using SARIMA forecast
+    # Label outliers using forecast
     for t in range(len(test)):
         obs = test[t]
         yhat = predictions_with_dates[test.index[t]]
-        print('predicted=%f, expected=%f' % (yhat, obs))
-        if abs(yhat - obs) > std:  #TODO: decide std scheme
+        if verbose:
+            print('predicted=%f, expected=%f' % (yhat, obs))
+        if abs(yhat - obs) > threshold:
             time_series_with_outliers.at[ts.index[t], 'Outlier'] = 'True'
             outlier = pd.Series(obs, index=[test.index[t]])
             outliers = outliers.append(outlier)
 
-    model_error = mean_squared_error(X[1:len(X)], predictions_with_dates)
-    test_error = mean_squared_error(test, predictions_with_dates[test.index[0]:test.index[-1]])
+    try:
+        # TODO: use RMSE instead of MSE?
+        model_error = mean_squared_error(X[1:len(X)], predictions_with_dates)
+        test_error = mean_squared_error(test, predictions_with_dates[test.index[0]:test.index[-1]])
+        print('Test MSE: %.3f' % test_error)
+        print('MSE: %.3f' % model_error)
+    except Exception as e:
+        print('Forecast error calculation failed:')
+        print(e)
 
-    print('Test MSE: %.3f' % test_error)
-    print('MSE: %.3f' % model_error)
-
-    # plot the forecast TODO: move this under verbose
-    X.plot(color='black', title='Dataset with Forecast and Outliers')  #TODO: pass in dataset name?
+    # plot the forecast TODO: move this under "if verbose"
+    X.plot(color='black', title='Time Series with Forecast and Outliers')
     test.plot(color='blue')
     predictions_with_dates.plot(color='green')
     if len(outliers) > 0:
-        print('Outliers: ' + str(len(outliers)) + "\n")
+        print('Outliers: ' + str(len(outliers)) + '\n')
         outliers.plot(color='red', style='.')
+    pyplot.legend(loc='best')
     pyplot.show()
 
     return time_series_with_outliers, outliers
