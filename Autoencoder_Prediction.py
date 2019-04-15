5bcabaff952bceb41dea78b8c22b738f1212b98a
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2019-04-15 15:52:18 -0600

    Experimentally found a network architecture that works well for all 5 DSs

--- Autoencoder_Prediction.py
+++ Autoencoder_Prediction.py
@@ -11,8 +11,11 @@ from keras.layers import Input, Dense
 from keras.models import Model
 from keras.models import load_model
 from keras.callbacks import TensorBoard
+from keras.layers.advanced_activations import LeakyReLU
 import numpy as np
 from tensorflow import set_random_seed
+from sklearn import preprocessing
+from sklearn.preprocessing import normalize
 
 
 __author__ = 'Shawn Polson'
@@ -61,22 +64,27 @@ class AutoEncoder:
     # TODO: with shingles of 18, the shape of this net is (18,10,18). Make it (18,150,75,10,75,150,18)
     def __init__(self, data, encoding_dim=3):
         self.encoding_dim = encoding_dim
-        # r = lambda: np.random.randint(1, 3)
+        r = lambda: np.random.randint(1, 3)
         # self.x = np.array([[r(), r(), r()] for _ in range(1000)])
+        # self.x = np.array([[r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r(), r()] for _ in range(1000)])
         self.x = data
         print(self.x)
 
     def _encoder(self):
         inputs = Input(shape=self.x[0].shape)
-        encoded = Dense(self.encoding_dim, activation='relu')(inputs)
+        #h1 = Dense(150, activation='tanh')(inputs)
+        h2 = Dense(75, activation='tanh')(inputs)
+        encoded = Dense(self.encoding_dim, activation='tanh')(h2)
         model = Model(inputs, encoded)
         self.encoder = model
         return model
 
     def _decoder(self):
         inputs = Input(shape=(self.encoding_dim,))
+        h3 = Dense(75, activation='tanh')(inputs)
+        # h4 = Dense(150, activation='tanh')(h3)
         num_outputs = self.x[0].shape[0]
-        decoded = Dense(num_outputs)(inputs)
+        decoded = Dense(num_outputs)(h3)
         model = Model(inputs, decoded)
         self.decoder = model
         return model
@@ -91,6 +99,7 @@ class AutoEncoder:
         model = Model(inputs, dc_out)
 
         self.model = model
+        print(self.model.summary())
         return model
 
     def fit(self, batch_size=10, epochs=300):
@@ -144,6 +153,13 @@ def autoencoder_prediction(dataset_path, train_size, path_to_model=None, var_nam
     print('Reading the dataset: ' + dataset_path)
     time_series = read_csv(dataset_path, header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
 
+    # Normalize data values between 0 and 1
+    X = time_series.values.reshape(-1, 1)
+    min_max_scaler = preprocessing.MinMaxScaler()
+    X_scaled = min_max_scaler.fit_transform(X).reshape(1, -1).tolist()[0]
+    normalized = pd.Series(X_scaled, index=time_series.index)
+    time_series = normalized
+
     # Shingle the dataset
     window_size = 18
     shingled_ts = shingle(time_series, window_size)
@@ -160,32 +176,39 @@ def autoencoder_prediction(dataset_path, train_size, path_to_model=None, var_nam
     seedy(2)
     ae = AutoEncoder(shingled_ts, encoding_dim=10)
     ae.encoder_decoder()
-    ae.fit(batch_size=50, epochs=300)
+    ae.fit(batch_size=50, epochs=1000)
     ae.save()
 
-    encoder = load_model(r'weights/encoder_weights.h5')
-    decoder = load_model(r'weights/decoder_weights.h5')
+    encoder = ae.encoder  # load_model(r'weights/encoder_weights.h5')
+    decoder = ae.decoder  # load_model(r'weights/decoder_weights.h5')
 
-    # print('\nDone fitting the autoencoder. Testing it with an arbitrary input:')
-    #
-    # #inputs = np.array([[2, 1, 1]])
-    # #inputs = np.array([[30, 31, 32, 30, 28, 31, 32, 34, 32, 32, 31, 31, 31, 30, 30, 15, 31, 31 ]])
-    # inputs = np.array([[2, 1, 100, 20, 50, 3, 3, 3, 3, 2, 3, 3, 3, 0, 30, 5, 31, 31]])
-    # x = encoder.predict(inputs)
-    # y = decoder.predict(x)
-    #
-    # print('Input: {}'.format(inputs))
-    # print('Encoded: {}'.format(x))
-    # print('Decoded: {}'.format(y))
-    #
-    # print('\nActually predicting the time series now.')
+    print('\nDone fitting the autoencoder. Testing it with an arbitrary input:')
+
+    #inputs = np.array([[2, 1, 1]])
+    inputs = np.array([[30, 31, 32, 30, 28, 31, 32, 34, 32, 32, 31, 31, 31, 30, 30, 15, 31, 31 ]])
+    #inputs = np.array([[2, 1, 100, 20, 50, 3, 3, 3, 3, 2, 3, 3, 3, 0, 30, 5, 31, 31]])
+    x = encoder.predict(inputs)
+    y = decoder.predict(x)
+
+    print('Input: {}'.format(inputs))
+    print('Encoded: {}'.format(x))  # NOTE: if network always encodes to 0s, the relu's have "died"
+    print('Decoded: {}'.format(y))
+
+    print('\nActually predicting the time series now.')
 
     predictions = time_series.values[:len(time_series)%window_size].tolist()
-
-    autoencoder_predictions = get_autoencoder_predictions(encoder, decoder, shingled_ts)
-
+    autoencoder_predictions = get_autoencoder_predictions(encoder, decoder, shingled_ts)  # feed data through network
     predictions = predictions + autoencoder_predictions
     predictions = pd.Series(predictions, index=time_series.index)
+
+    #model_predict = ae.model.predict(np.array([shingled_ts[0]]))
+    s = np.array([shingled_ts[-1]])
+    encoded = encoder.predict(s)
+    decoded = decoder.predict(encoded)
+    print('Model prediction on last shingle:')
+    print('Input: {}'.format(s))
+    print('Encoded: {}'.format(encoded))  # NOTE: if network always encodes to 0s, the relu's have "died"
+    print('Decoded: {}'.format(decoded))
 
     return time_series, predictions
 
@@ -194,7 +217,7 @@ def autoencoder_prediction(dataset_path, train_size, path_to_model=None, var_nam
 if __name__ == "__main__":
     print('Autoencoder_Prediction.py is being run directly\n')
 
-    ds_num = 0  # used to select dataset path and variable name together
+    ds_num = 3  # used to select dataset path and variable name together
 
     dataset = ['Data/BusVoltage.csv', 'Data/TotalBusCurrent.csv', 'Data/BatteryTemperature.csv',
                'Data/WheelTemperature.csv', 'Data/WheelRPM.csv'][ds_num]
@@ -203,6 +226,11 @@ if __name__ == "__main__":
     time_series, predictions = autoencoder_prediction(dataset_path=dataset, var_name=name,
                                                       train_size=0.5, verbose=True)
 
+    # Plot time series with autoencoder predictions
+    time_series.plot(color='black', title='Time Series with Autoencoder Predictions', label=name)
+    predictions.plot(color='blue', label='Predictions')
+    pyplot.legend(loc='best')
+    pyplot.show()
 
 
 else:
