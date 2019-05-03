cee5e801e40c3a7e6a4776d7d13eb695b3c5a717
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2019-05-03 12:24:46 -0600

    Optionally don't calculate errors

--- model_with_rolling_mean.py
+++ model_with_rolling_mean.py
@@ -9,7 +9,7 @@ __author__ = 'Shawn Polson'
 __contact__ = 'shawn.polson@colorado.edu'
 
 
-def model_with_rolling_mean(ts, window, verbose, var_name='Value'):
+def model_with_rolling_mean(ts, window, verbose, var_name='Value', errors=False):
     """Model the time series data with a rolling mean.
 
        Inputs:
@@ -20,13 +20,13 @@ def model_with_rolling_mean(ts, window, verbose, var_name='Value'):
        Optional Inputs:
            var_name [str]: The name of the dependent variable in the time series.
                            Default is 'Value'.
+           errors [bool]:  Whether or not to calculate and return errors between data and rolling mean.
 
        Outputs:
            rolling_mean [pd Series]: The rolling mean, as a pandas Series with a DatetimeIndex and a column for the rolling mean.
-           errors [pd Series]:       The errors at each point, as a pandas Series with a DatetimeIndex and a column for the errors.
 
        Optional Outputs:
-           None
+           errors [pd Series]: The errors at each point, as a pandas Series with a DatetimeIndex and a column for the errors.
 
        Example:
            rolling_mean, errors = detect_anomalies_with_rolling_mean(time_series, window_size, False)
@@ -47,26 +47,29 @@ def model_with_rolling_mean(ts, window, verbose, var_name='Value'):
     # TODO: save data & plots to proper directories with encoded file names
     if verbose:
         # TODO: finalize coloring
-        pyplot.plot(ts, color='black', label='Time Series')
-        pyplot.plot(rolling_mean, color='blue', label='Rolling Mean')
+        pyplot.plot(ts, color='black', label=var_name)
+        pyplot.plot(rolling_mean, color='cyan', label='Rolling Mean')
         pyplot.legend(loc='best')
         pyplot.title('Time Series & Rolling Mean')
         pyplot.show()
 
-    # Start a progress bar
-    widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.Timer(), ' ', progressbar.AdaptiveETA()]
-    progress_bar_sliding_window = progressbar.ProgressBar(
-        widgets=[progressbar.FormatLabel('Rolling Mean errors ')] + widgets,
-        maxval=int(len(X))).start()
+    if errors:
+        # Start a progress bar
+        widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.Timer(), ' ', progressbar.AdaptiveETA()]
+        progress_bar_sliding_window = progressbar.ProgressBar(
+            widgets=[progressbar.FormatLabel('Rolling Mean errors ')] + widgets,
+            maxval=int(len(X))).start()
 
-    # Get errors
-    for t in range(len(X)):
-        obs = X[t]
-        y = rolling_mean[t]
-        error = abs(y-obs)
-        error_point = pd.Series(error, index=[ts.index[t]])
-        errors = errors.append(error_point)
+        # Get errors
+        for t in range(len(X)):
+            obs = X[t]
+            y = rolling_mean[t]
+            error = abs(y-obs)
+            error_point = pd.Series(error, index=[ts.index[t]])
+            errors = errors.append(error_point)
 
-        progress_bar_sliding_window.update(t)  # advance progress bar
+            progress_bar_sliding_window.update(t)  # advance progress bar
 
-    return rolling_mean, errors
+        return rolling_mean, errors
+    else:
+        return rolling_mean
