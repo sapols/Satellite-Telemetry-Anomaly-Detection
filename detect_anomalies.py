226ff75fa9a11007ac7bd55eb31a2949ea6a1ad1
Shawn Polson <Shawn.Polson@lasp.colorado.edu>
2019-05-16 16:42:48 -0600

    Fully support saving plots and data

--- detect_anomalies.py
+++ detect_anomalies.py
@@ -2,6 +2,7 @@
 import os
 import progressbar
 import pandas as pd
+from pandas import datetime
 import numpy as np
 from matplotlib import pyplot
 
@@ -12,41 +13,53 @@ __author__ = 'Shawn Polson'
 __contact__ = 'shawn.polson@colorado.edu'
 
 
-def detect_anomalies(ts, normal_model, var_name, ds_name, outlier_def='std', num_stds=2, verbose=False):
+def parser(x):
+    new_time = ''.join(x.split('.')[0])  # remove microseconds from time data
+    try:
+        return datetime.strptime(new_time, '%Y-%m-%d %H:%M:%S')  # for bus voltage, battery temp, wheel temp, and wheel rpm data
+    except:
+        return datetime.strptime(new_time, '%Y-%m-%d')  # for total bus current data
+
+
+def detect_anomalies(ts, normal_model, ds_name, var_name, alg_name, outlier_def='std', num_stds=2,
+                     plot_save_path=None, data_save_path=None):
     """Detect outliers in the time series data by comparing points against a "normal" model.
 
        Inputs:
            ts [pd Series]:           A pandas Series with a DatetimeIndex and a column for numerical values.
-           normal_model [pd Series]: ts [pd Series]:   A pandas Series with a DatetimeIndex and a column for numerical values.
-           var_name [str]:           The name of the dependent variable in the time series.
+           normal_model [pd Series]: A pandas Series with a DatetimeIndex and a column for numerical values.
            ds_name [str]:            The name of the time series dataset.
-
+           var_name [str]:           The name of the dependent variable in the time series.
+           alg_name [str]:           The name of the algorithm used to create 'normal_model'.
 
        Optional Inputs:
-           outlier_def [str]: {'std', 'errors', 'dynamic'} The definition of an outlier to be used. Can be 'std' for [num_stds] from the data's mean,
-                              'errors' for [num_stds] from the mean of the errors, or 'dynamic' for nonparametric dynamic thresholding
-                              Default is 'std'.
-           num_stds [float]:  The number of standard deviations away from the mean used to define point outliers (when applicable).
-                              Default is 2.
-           verbose [bool]:
+           outlier_def [str]:    {'std', 'errors', 'dynamic'} The definition of an outlier to be used. Can be 'std' for [num_stds] from the data's mean,
+                                 'errors' for [num_stds] from the mean of the errors, or 'dynamic' for nonparametric dynamic thresholding
+                                 Default is 'std'.
+           num_stds [float]:     The number of standard deviations away from the mean used to define point outliers (when applicable).
+                                 Default is 2.
+           plot_save_path [str]: The file path (ending in file name *.png) for saving plots of outliers.
+           data_save_path [str]: The file path (ending in file name *.csv) for saving CSVs with outliers.
 
        Outputs:
-           time_series_with_outliers [pd DataFrame]: A pandas DataFrame with a DatetimeIndex, a columns for numerical values, and an Outlier column (True or False).
+           time_series_with_outliers [pd DataFrame]: A pandas DataFrame with a DatetimeIndex, two columns for numerical values, and an Outlier column (True or False).
 
        Optional Outputs:
            None
 
        Example:
-           time_series_with_outliers = detect_anomalies(time_series, model,
-                                                        'Temperature (C)', 'BatteryTemperature', 'dynamic')
+           time_series_with_outliers = detect_anomalies(time_series, model, 'BatteryTemperature', 'Temperature (C)',
+                                                        'ARIMA', 'dynamic', plot_path, data_path)
     """
 
     X = ts.values
     Y = normal_model.values
     outliers = pd.Series()
     errors = pd.Series()
-    time_series_with_outliers = pd.DataFrame({var_name: ts})
+    time_series_with_outliers = pd.DataFrame({var_name: ts, alg_name: normal_model})
     time_series_with_outliers['Outlier'] = 'False'
+    column_names = [var_name, alg_name, 'Outlier']  # column order
+    time_series_with_outliers = time_series_with_outliers.reindex(columns=column_names)  # sort columns in specified order
 
     # Start a progress bar
     widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.Timer(), ' ', progressbar.AdaptiveETA()]
@@ -112,24 +125,175 @@ def detect_anomalies(ts, normal_model, var_name, ds_name, outlier_def='std', num
                 outlier = pd.Series(X[i], index=[ts.index[i]])
                 outliers = outliers.append(outlier)
 
+    # Plot anomalies
+    ax = ts.plot(color='#192C87', title=ds_name + ' with ' + alg_name + ' Outliers', label=var_name, figsize=(14, 6))
+    normal_model.plot(color='#0CCADC', label=alg_name, linewidth=1)
+    if len(outliers) > 0:
+        print('\nDetected outliers (' + ds_name + '): ' + str(len(outliers)) + '\n')
+        outliers.plot(color='red', style='.', label='Outliers')
+    ax.set(xlabel='Time', ylabel=var_name)
+    pyplot.legend(loc='best')
 
-    # TODO: save data and plots
-    # Save plot to proper directory with encoded file name
-    ax = ts.plot(color='#192C87', title=ds_name + ' with Rolling Mean', label=var_name, figsize=(14, 6))
-    # TODO: pass in a str for the algorithm used to get model, for the label below
-    normal_model.plot(color='#0CCADC', label='Model', linewidth=1.5)
+    # Save plot
+    if plot_save_path is not None:
+        plot_dir = plot_save_path[:plot_save_path.rfind('/')+1]
+        if not os.path.exists(plot_dir):
+            os.makedirs(plot_dir)
+        pyplot.savefig(plot_save_path, dpi=500)
+
+    pyplot.show()
+    pyplot.clf()
+
+    # Save data
+    if data_save_path is not None:
+        data_dir = data_save_path[:data_save_path.rfind('/')+1]
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        time_series_with_outliers.to_csv(data_save_path)
+
+    return time_series_with_outliers
+
+
+def detect_anomalies_with_many_stds(ts, normal_model, var_name, ds_name, alg_name, outlier_def='std', standard_deviations=[2,4,8],
+                                    plot_save_path=None, data_save_path=None):
+                     # TODO: add standard_deviations param (list of 3 floats) and when it isn't None, plot with all of them (red, orangered, orange), save 3 CSVs
+    """Detect outliers in the time series data by comparing points against a "normal" model, using a set of three standard deviations as thresholds.
+
+       Inputs:
+           ts [pd Series]:           A pandas Series with a DatetimeIndex and a column for numerical values.
+           normal_model [pd Series]: A pandas Series with a DatetimeIndex and a column for numerical values.
+           var_name [str]:           The name of the dependent variable in the time series. # TODO: can this come from ts.columns?? (make optional?)
+           ds_name [str]:            The name of the time series dataset.
+           alg_name [str]:           The name of the algorithm used to create 'normal_model'. # TODO: can this come from normal_model.columns?? (make optional?)
+
+       Optional Inputs:
+           outlier_def [str]:    {'std', 'errors'} The definition of an outlier to be used. Can be 'std' for [num_stds] from the data's mean
+                                 or 'errors' for [num_stds] from the mean of the errors.
+                                 Default is 'std'.
+           standard_deviations   [list of 3 floats]: Exactly 3 numbers which will be successively used as the number of standard deviations away from the mean that constitutes an outlier. 
+                                 Plots will show outliers with all 3 stds and 3 CSVs will be saved.
+                                 Default is [2,4,6].
+           plot_save_path [str]: The file path (ending in file name) for saving plots of outliers.
+           data_save_path [str]: The file path (ending in file name) for saving 
+
+       Outputs:
+           time_series_with_outliers [pd DataFrame]: A pandas DataFrame with a DatetimeIndex, a columns for numerical values, and an Outlier column (True or False).
+
+       Optional Outputs:
+           None
+
+       Example:
+           time_series_with_outliers = detect_anomalies(time_series, model, 'Temperature (C)', 
+                                                        'BatteryTemperature', 'ARIMA', 'dynamic',
+                                                        plot_path, data_path)
+    """
+
+    X = ts.values
+    Y = normal_model.values
+    outliers1 = pd.Series()
+    outliers2 = pd.Series()
+    outliers3 = pd.Series()
+    errors = pd.Series()
+    time_series_with_outliers = pd.DataFrame({var_name: ts, alg_name: normal_model})  # TODO: make sure this works, try normal_model.columns for alg_name
+    time_series_with_outliers['Outlier'] = 'False'
+
+    # Start a progress bar
+    widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.Timer(), ' ', progressbar.AdaptiveETA()]
+    progress_bar_sliding_window = progressbar.ProgressBar(
+        widgets=[progressbar.FormatLabel('Outliers (' + ds_name + ')')] + widgets,
+        maxval=int(len(X))).start()
+
+    # Define outliers by distance from "normal" model
+    if outlier_def == 'std':
+        # Label outliers using standard deviations
+        std = float(X.std(ddof=0))
+        for t in range(len(X)):
+            obs = X[t]
+            y = Y[t]
+            error = abs(y - obs)
+            error_point = pd.Series(error, index=[ts.index[t]])
+            errors = errors.append(error_point)
+            if error > std*num_stds:
+                time_series_with_outliers.at[ts.index[t], 'Outlier'] = 'True'
+                outlier = pd.Series(obs, index=[ts.index[t]])
+                outliers = outliers.append(outlier)
+            progress_bar_sliding_window.update(t)  # advance progress bar
+
+    # Define outliers by distance from mean of errors
+    elif outlier_def == 'errors':
+        # Populate errors
+        for t in range(len(X)):
+            obs = X[t]
+            y = Y[t]
+            error = abs(y - obs)
+            error_point = pd.Series(error, index=[ts.index[t]])
+            errors = errors.append(error_point)
+
+            progress_bar_sliding_window.update(t)  # advance progress bar
+
+        mean_of_errors = float(errors.values.mean())
+        std_of_errors = float(errors.values.std(ddof=0))
+        threshold = mean_of_errors + (std_of_errors*num_stds)
+
+        # Label outliers using standard deviations from the errors' mean
+        for t in range(len(X)):
+            obs = X[t]
+            y = Y[t]
+            error = errors[t]
+            if error > threshold:
+                time_series_with_outliers.at[ts.index[t], 'Outlier'] = 'True'
+                outlier = pd.Series(obs, index=[ts.index[t]])
+                outliers = outliers.append(outlier)
+            progress_bar_sliding_window.update(t)  # advance progress bar
+
+    # Save plot 
+    ax = ts.plot(color='#192C87', title=ds_name + ' with ' + alg_name + ' Outliers', label=var_name, figsize=(14, 6))
+    normal_model.plot(color='#0CCADC', label=alg_name, linewidth=1)
     if len(outliers) > 0:
         print('\nDetected outliers: ' + str(len(outliers)) + '\n')
         outliers.plot(color='red', style='.', label='Outliers')
     ax.set(xlabel='Time', ylabel=var_name)
     pyplot.legend(loc='best')
 
-    # plot_filename = ds_name + '_with_rolling_mean.png'
-    # plot_path = './save/datasets/' + ds_name + '/rolling mean/plots/'
-    # if not os.path.exists(plot_path):
-    #     os.makedirs(plot_path)
-    # pyplot.savefig(plot_path + plot_filename, dpi=500)
+    plot_dir = plot_save_path[:plot_save_path.rfind('/')+1]  # TODO: make sure this drops the file name and keeps the slash
+    if not os.path.exists(plot_dir): 
+        os.makedirs(plot_dir)    
+    #pyplot.savefig(plot_save_path, dpi=500)  # TODO: make sure works, Uncomment saving
 
     pyplot.show()
+    pyplot.clf()  # TODO: make sure works
+
+    # Save data
+    data_dir = data_save_path[:data_save_path.rfind('/')+1]  # TODO: make sure this drops the file name and keeps the slash
+    if not os.path.exists(data_dir): 
+        os.makedirs(data_dir)  
+    #time_series_with_outliers.to_csv(data_save_path)  # TODO: Uncomment saving
+
 
     return time_series_with_outliers
+
+
+
+if __name__ == "__main__":
+
+    datasets = ['Data/BusVoltage.csv', 'Data/TotalBusCurrent.csv', 'Data/BatteryTemperature.csv',
+                'Data/WheelTemperature.csv', 'Data/WheelRPM.csv']
+
+    # Rolling Mean
+    for ds in range(len(datasets)):
+        ds_name = datasets[ds][5:-4]  # drop 'Data/' and '.csv'
+
+        file = 'save/datasets/' + ds_name + '/rolling mean/data/' + ds_name + '_with_rolling_mean.csv'
+        ts_with_model = pd.read_csv(file, header=0, parse_dates=[0], index_col=0, date_parser=parser)
+        var_name = ts_with_model.columns[0]
+        alg_name = ts_with_model.columns[1]
+
+        x = ts_with_model[var_name]
+        y = ts_with_model[alg_name]
+
+        ts_with_outliers = detect_anomalies(x, y, ds_name, var_name, alg_name=alg_name, outlier_def='std', num_stds=2,
+                                            plot_save_path='./test/plot.png', data_save_path='./test/data.csv')
+
+
+else:
+    print('\n')
